73ed6f59ac916f9022eff018d3c5200bd785b1c6
Dawid Dęby <dawid.deby@gmail.com>
2021-02-02 12:20:38 +0100

    Create deck of card

--- lib/cards/card.ex
+++ lib/cards/card.ex
@@ -15,5 +15,7 @@ defmodule Cards.Card do
 
   defp get_power_by_value(value) when value == "A", do: 11
   defp get_power_by_value(value) when value in ["J", "Q", "K"], do: 10
-  defp get_power_by_value(value) when value in 1..10, do: value
+
+  defp get_power_by_value(value) when value in ~w"1 2 3 4 5 6 7 8 9 10",
+    do: String.to_integer(value)
 end
--- lib/cards/deck.ex
+++ lib/cards/deck.ex
@@ -1,7 +1,12 @@
 defmodule Cards.Deck do
   alias Cards.Card
-
+  @values ~w[2 3 4 5 6 7 8 9 10 J Q K A]
+  @suits ~w[club heart spade diamond]
   def create_card(suit, value) do
     Card.new(suit, value)
+  end
+
+  def create_deck do
+    for suit <- @suits, value <- @values, do: Card.new(suit, value)
   end
 end
